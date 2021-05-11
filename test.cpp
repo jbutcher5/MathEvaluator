f7a16175accee864d48799950633367af8b6b61f
KJ002 <jamesbutcher167@gmail.com>
2021-05-11 20:25:29 +0100

    cleaned up test

--- test.cpp
+++ test.cpp
@@ -20,6 +20,8 @@ bool eval(std::vector<std::string> input, std::vector<std::string> RPN, std::vec
 
     std::vector<mp_RPN> RPN_Values;
 
+    LOG("Convertion");
+
     for (int x = 0; x < (int)input.size(); x++){
       mp_RPN u = parser.reversePolishNotation(input[x]);
       if (u.RPN == RPN[x]){
@@ -34,7 +36,7 @@ bool eval(std::vector<std::string> input, std::vector<std::string> RPN, std::vec
       RPN_Values.push_back(u);
     }
 
-    std::cout << (rpn_score/(int)input.size())*100 << "%" << std::endl;
+    LOG("Evaluation");
 
     for (int x = 0; x < (int)input.size(); x++){
       double evaluation = parser.eval(RPN_Values[x]);
@@ -49,8 +51,6 @@ bool eval(std::vector<std::string> input, std::vector<std::string> RPN, std::vec
       }
     }
 
-    std::cout << (evaluation_score/(int)input.size())*100 << "%" << std::endl;
-
     if (rpn_score == (int)input.size() && evaluation_score == (int)input.size()) return false;
 
   }
@@ -61,7 +61,7 @@ bool eval(std::vector<std::string> input, std::vector<std::string> RPN, std::vec
 
 int main(){
   std::vector<std::string> input = {"1+1", "4+8*3", "-5+20*30", "(4+8)*3", "6+9+(4*2+4^2)", "2*(1+(4*(2+1)+3))", "(5.9-5.3)*7.2+1.4^2", "2*20/2+(3+4)*3^2-6+15", "sin(1.5707963267948966)"};
-  std::vector<std::string> RPN = {"11+", "483*+", "5-2030*+", "48+3*", "69+42*42^++", "21421+*3++*", "5.95.3-7.2*1.42^+", "220*2/34+32^*+6-15+", "1.5707963267948966sin"};
+  std::vector<std::string> RPN = {"11+", "483*+", "-52030*+", "48+3*", "69+42*42^++", "21421+*3++*", "5.95.3-7.2*1.42^+", "220*2/34+32^*+6-15+", "1.5707963267948966sin"};
   std::vector<double> result = {2, 28, 595, 36, 39, 32, 6.28, 92, 1};
 
   return eval(input, RPN, result);
